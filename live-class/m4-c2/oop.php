0b521f9a68efa85bba320f46de59d2a387f04632
Oline IT <amisalabir@gmail.com>
2023-10-02 10:54:55 +0600

    Module 4

---
+++ live-class/m4-c2/oop.php
@@ -0,0 +1,5 @@
+<?php
+//Enhancing Code Quality with OOP Features
+
+//Better code with OOP by forcing | Interfaces & Its practical Example | Magic Methods, Object Cloning, Conversion, Comparing | Detailed discussion on early and late binding | Namespaces, Method Overloading, Overridingå
+
