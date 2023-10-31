afa7f6a5a47fa2955fbba7b20b0e254b91bf9d0f
Oline IT <amisalabir@gmail.com>
2023-10-31 18:57:41 +0600

    Assignment-5

---
+++ live-test/code-mama/cm-5.php
@@ -0,0 +1,43 @@
+<?php
+    # Write your PHP code from here
+    fscanf(STDIN,"%d %d", $a, $b);
+
+    function lookforten(int $x, int $y){
+       $z=$x+$y;
+        if($x==10 || $y==10 || $z==10){
+        echo "True";        }
+        else{
+            echo "False";
+        }
+        
+    }
+    lookforten($a,$b);
+
+
+      # Write your PHP code from here
+      fscanf(STDIN,"%d %d", $a, $b);
+
+      function lookforten(int $x, int $y){
+         $z=$x+$y;
+          if($x==10 || $y==10 || $z==10){
+          echo "True";        }
+          else{
+              echo "False";
+          }
+          
+      }
+      lookforten($a,$b);
+  ###################
+     # Write your PHP code from here
+     fscanf(STDIN,"%d %d", $a, $b);
+
+     function lookforten(int $x, int $y){
+        $z=$x+$y;
+         if($x==10 || $y==10 || $z==10){
+         echo "True";        }
+         else{
+             echo "False";
+         }
+         
+     }
+     lookforten($a,$b);
